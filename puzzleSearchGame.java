135c503df83efd3e54d40242b02fe9f19b633a28
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2022-05-30 18:51:42 -0700

    added wordBank functionality to puzzleSearchGame.java

--- puzzleSearchGame.java
+++ puzzleSearchGame.java
@@ -1,11 +1,11 @@
 import java.util.Scanner; 
+import java.util.ArrayList; 
 public class puzzleSearchGame {
+  private ArrayList<String> wordBank = new ArrayList<String>(); 
+  private int minGridSize = 10; //10 by 10 square 
+  private final int MAX_GRID_SIZE = 30; //30 by 30 square 
 
-  public static void runProgram(){
-    new puzzleSearchGame().programExecute();
-  } 
-
-  private void programExecute(){
+  public void runProgram(){
     printInstructions(); 
     processUserMenuInput(); 
   } 
@@ -30,7 +30,7 @@ public class puzzleSearchGame {
     while(!checkIfValidLetter(userInput)){
       userInput = askForValidInput();
     }
-
+    executeOption(userInput);
   } 
 
   private boolean checkIfValidLetter(String userInput){
@@ -52,6 +52,46 @@ public class puzzleSearchGame {
     Scanner scannerObject = new Scanner(System.in); 
     System.out.println("Your input is invalid, please consider re-typing in the following options: ");
     printInputOptions();
+    String userInput = scannerObject.next(); 
+    return userInput; 
+  } 
+
+  private void executeOption(String option){
+    switch(option){
+      case "g":
+        generateWordSearch(); 
+      default: 
+        System.out.println("awd"); 
+    }
+  } 
+
+  private void generateWordSearch(){
+    //ask user repeatedly, the words they want to include. 
+    //set up a counter, to get which word is the longest, 
+    //set up the grid at the longest word's size 
+    //if words don't fit the grid no more, and grid size is still less than max grid size, increase grid size. 
+
+    //if lonest word is less than 10, size is 10 
+    //if longest word is more than 10 and less than 30, grid size depends on number of words and if they fit 
+    collectWords(); 
+    
+    
+  } 
+
+  private void collectWords(){
+    System.out.println("Please enter your word, type in '0' when you're done");
+    String userInput = askUserForWords(); 
+    while(userInput != "0"){
+      this.wordBank.add(userInput); 
+      userInput = askUserForWords(); 
+    } 
+    System.out.println(this.wordBank.toString());  
+  } 
+
+  private String askUserForWords(){
+    Scanner scannerObject = new Scanner(System.in);
+    int wordCount = this.wordBank.size(); 
+    System.out.println("enter word " + wordCount + ": ");
     String userInput = scannerObject.next(); 
     return userInput; 
   } 
--- tsaqifWordSearch.java
+++ tsaqifWordSearch.java
@@ -1,6 +1,5 @@
 public class tsaqifWordSearch{
   public static void main(String[] args){
-    System.out.println("kontol"); 
     new puzzleSearchGame().runProgram(); 
   }
 }
