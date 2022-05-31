5a39390316fe81a28ecd265654cd94a6ad3e490c
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2022-05-30 18:08:44 -0700

    added user-input parsing functionality to puzzleSearchGame.java

--- puzzleSearchGame.java
+++ puzzleSearchGame.java
@@ -1,3 +1,58 @@
+import java.util.Scanner; 
 public class puzzleSearchGame {
+
+  public static void runProgram(){
+    new puzzleSearchGame().programExecute();
+  } 
+
+  private void programExecute(){
+    printInstructions(); 
+    processUserMenuInput(); 
+  } 
   
+  public void printInstructions(){
+    System.out.println("Welcome to my word search generator!");
+    System.out.println("This program will allow you to generate your own word search puzzle");
+    System.out.println("Please select an option: ");
+    System.out.println("\t Generate a new word search (g)");
+    System.out.println("\t Print out your word search (p)");
+    System.out.println("\t Show the solution to your word search (s)");
+    System.out.println("\t Quit the program (q)");
+  }
+
+  private void processUserMenuInput(){
+    Scanner scannerObject = new Scanner(System.in); 
+    String userInput = scannerObject.next(); 
+    while(!checkIfValidLetter(userInput)){
+      userInput = askForValidInput();
+    }
+
+  } 
+
+  private boolean checkIfValidLetter(String userInput){
+    switch(userInput.toLowerCase()){
+      case "g":
+        return true; 
+      case "p":
+        return true; 
+      case "s":
+        return true; 
+      case "q":
+        return true; 
+      default: 
+        return false; 
+    }
+  } 
+
+  private String askForValidInput(){
+    Scanner scannerObject = new Scanner(System.in); 
+    System.out.println("Your input is invalid, please consider re-typing in the following options: ");
+    System.out.println("\t Generate a new word search (g)");
+    System.out.println("\t Print out your word search (p)");
+    System.out.println("\t Show the solution to your word search (s)");
+    System.out.println("\t Quit the program (q)");
+    String userInput = scannerObject.next(); 
+    return userInput; 
+  } 
+
 }
--- tsaqifWordSearch.java
+++ tsaqifWordSearch.java
@@ -1,5 +1,6 @@
 public class tsaqifWordSearch{
   public static void main(String[] args){
     System.out.println("kontol"); 
+    new puzzleSearchGame().runProgram(); 
   }
 }
